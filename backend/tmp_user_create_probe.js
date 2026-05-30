44eab8861e377abe3f905350c40a016e14a1d394
Varshitha2424 <varshitharagalla@gmail.com>
2026-05-30 17:37:00 +0530

    Delete backend/tmp_user_create_probe.js

--- backend/tmp_user_create_probe.js
+++
@@ -1,8 +0,0 @@
-import UserModel from './src/models/user.model.js';
-
-try {
-  const result = await UserModel.create({ email: 'probe_' + Date.now() + '@example.com', password_hash: 'hashed_pw', first_name: 'Probe', last_name: 'User', role: 'member' });
-  console.log('INSERT OK', result);
-} catch (e) {
-  console.error('ERR', e.code || e.message, e);
-}
